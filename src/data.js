fc4b4e54ede0d790878344ea0aa01fd74422113c
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2020-12-03 16:35:04 -0300

    correções

--- src/data.js
+++ src/data.js
@@ -1,9 +1,33 @@
-// estas funciones son de ejemplo
+export const ordenarAaZ = (pokemonGo, filtroOrdem) => {
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+  if (filtroOrdem == "a-z") {
+  console.log(filtroOrdem)
+  return pokemonGo.sort(function(a, b) {
+  
+  if (a.name <= b.name) {
+  return -1
+  } else {
+  return 1;
+  }
+  });
+  } else {
+  
+  return pokemonGo.sort(function(a, b) {
+  
+  if (a.name <= b.name) {
+  return 1;
+  } else {
+  return -1;
+  }
+  })
+  }
+  }
+  
+  export const buscarTipo = (pokemonGo, filtroTipo) => pokemonGo.filter((tipo) =>
+  (tipo.type.includes(filtroTipo)));
+  export const porNome = (pokemonGo, pokeName) => pokemonGo.filter((nome) =>
+  (nome.name.toLowerCase().indexOf(pokeName.toLowerCase()) > -1));
+  
+  export const resultado = (pokemonGo, filtroTipo) =>
+  ((filtroTipo) / (pokemonGo) * 100);
+  
